268640f23297160eab3a94310b9f842325f7759a
ClaraCamejo <clara.camejo@hotmail.com>
2020-11-24 19:14:12 -0300

    Header y menu hamburguesa

--- spoiler-five/src/app/app.module.ts
+++ spoiler-five/src/app/app.module.ts
@@ -3,14 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { MenuMobileComponent } from './menu-mobile/menu-mobile.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
-    MenuMobileComponent
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
--- spoiler-five/src/app/nav-bar/nav-bar.component.ts
+++ spoiler-five/src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  visible = false;
 
-  constructor() { }
+  constructor(private _eref: ElementRef) { }
 
   ngOnInit(): void {
+    this.visible = false;
+  }
+
+  clickAddTodo() {
+    if (!this.visible) {
+      let menu = document.querySelector("#menu");
+      menu.style.right = 0;
+      this.visible = true;
+    }
+  }
+
+  cerrarMenu() {
+    if (this.visible) {
+      let menu = document.querySelector("#menu");
+      menu.style.right = '-300px';
+      this.visible = false;
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClick(event: Event) {
+    let menu = document.querySelector("#menu");
+    if (!this._eref.nativeElement.contains(event.target)) {
+      this.cerrarMenu();
+    }
   }
 
 }
