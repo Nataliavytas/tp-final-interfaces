2a95d6962dbf1bb49f75f0dce99f0f4bb9f49ba2
ClaraCamejo <clara.camejo@hotmail.com>
2020-11-23 23:37:51 -0300

    Menu lateral mobile

--- spoiler-five/src/app/app.module.ts
+++ spoiler-five/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MenuMobileComponent } from './menu-mobile/menu-mobile.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    MenuMobileComponent
   ],
   imports: [
     BrowserModule,
