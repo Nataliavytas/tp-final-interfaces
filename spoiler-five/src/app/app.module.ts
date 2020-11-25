8564754b3d73dfaa310b053faf17ea2d8c9981ed
Natalia Vytas <nvytas@temperies.co>
2020-11-24 23:30:05 -0300

    add side web menu & reproductor structure

--- spoiler-five/src/app/app.module.ts
+++ spoiler-five/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LandingNavbarComponent } from './landing-navbar/landing-navbar.component';
+import { LeftNavigationBarComponent } from './left-navigation-bar/left-navigation-bar.component';
+import { HomeComponent } from './home/home.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MusicReproductorComponent } from './music-reproductor/music-reproductor.component';
 
 
 @NgModule({
@@ -11,9 +15,13 @@ import { LandingNavbarComponent } from './landing-navbar/landing-navbar.componen
     AppComponent,
     LandingPageComponent,
     LandingNavbarComponent,
+    LeftNavigationBarComponent,
+    HomeComponent,
+    MusicReproductorComponent,
   ],
   imports: [
     BrowserModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
