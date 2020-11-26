314684a789a7a35a7dcf3985b37c7dc9c6362aa0
ClaraCamejo <clara.camejo@hotmail.com>
2020-11-26 19:01:40 -0300

    comportamiento reproductor desktop

--- spoiler-five/src/app/music-reproductor/music-reproductor.component.ts
+++ spoiler-five/src/app/music-reproductor/music-reproductor.component.ts
@@ -7,6 +7,12 @@ import { faChevronUp, faHeart, faList, faPlayCircle, faStar, faStepBackward, faS
   styleUrls: ['./music-reproductor.component.css']
 })
 export class MusicReproductorComponent implements OnInit {
+  color: string;
+  color2: string;
+  color3: string;
+  color4: string;
+  color5: string;
+  corazon: string;
 
   constructor() { }
   faVolume = faVolumeUp;
@@ -19,6 +25,63 @@ export class MusicReproductorComponent implements OnInit {
   faList = faList;
 
   ngOnInit(): void {
+    this.color = "#585A66";
+    this.color2 = "#585A66";
+    this.color3 = "#585A66";
+    this.color4 = "#585A66";
+    this.color5 = "#585A66";
+    this.corazon = "#585A66";
+  }
+
+  getCorazon() {
+    return this.corazon;
+  }
+
+  getcolor() {
+    return this.color;
+  }
+
+  getcolor2() {
+    return this.color2;
+  }
+  getcolor3() {
+    return this.color3;
+  }
+  getcolor4() {
+    return this.color4;
+  }
+  getcolor5() {
+    return this.color5;
+  }
+
+  clickCorazon() {
+    if (this.corazon == "#585A66") {
+      this.corazon = "#c27ef0";
+    } else {
+      this.corazon = "#585A66"
+    }
+
+  }
+
+  clickAddTodo(n) {
+    this.color = "#585A66";
+    this.color2 = "#585A66";
+    this.color3 = "#585A66";
+    this.color4 = "#585A66";
+    this.color5 = "#585A66";
+    for (let i = 1; i <= n; i++) {
+      if (i == 1) {
+        this.color = "gold";
+      } else if (i == 2) {
+        this.color2 = "gold";
+      } else if (i == 3) {
+        this.color3 = "gold";
+      } else if (i == 4) {
+        this.color4 = "gold";
+      } else if (i == 5) {
+        this.color5 = "gold";
+      }
+    }
   }
 
 }
